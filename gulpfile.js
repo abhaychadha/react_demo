471a26a5edfc086667ae4250bee83c542882831a
abhaychadha <abhay.chadha48@gmail.com>
2016-03-09 19:12:36 +0530

    committing gulp file changes

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('connect', function() {
 
 gulp.task('open', ['connect'], function() {
 	gulp.src('dist/index.html')
-		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/index.html'}));
 });
 
 gulp.task('html', function() {
